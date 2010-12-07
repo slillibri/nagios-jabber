ee2e5bba350186fbf6dc3363a583056a4d75fe7d
slillibri <scott.lillibridge@gmail.com>
2010-12-07 14:19:50 -0700

    Production commit

--- bot.rb
+++ bot.rb
@@ -108,4 +108,15 @@ class Bot
   end
 end
 
-#b = Bot.new(:botname =>  'bot@jabber.thereisnoarizona.org',:host =>  'jabber.thereisnoarizona.org',:password =>  'm0rph3us', :status_log => '/var/cache/nagios3/status.dat', :cmd_file => '/var/lib/nagios3/rw/nagios.cmd')
+orig_stdout = $stdout
+$stdout = File.new('/dev/null', 'w')
+pid = fork do
+  b = Bot.new(:botname =>  'bot@jabber.thereisnoarizona.org',:host =>  'jabber.thereisnoarizona.org',:password =>  'j4bb3rb0t!', :status_log => '/var/cache/nagios3/status.dat', :cmd_file => '/var/lib/nagios3/rw/nagios.cmd')
+end
+::Process.detach pid
+$stdout = orig_stdout
+File.open('/var/run/nagios-jabber.pid', 'w') do |f|
+  f.puts pid
+end
+
+
