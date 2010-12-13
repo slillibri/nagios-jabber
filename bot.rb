3d1f91ba212d019e5b45ef09e1cd91e8c7d615b9
slillibri <scott.lillibridge@gmail.com>
2010-12-13 15:04:09 -0700

    Stop overwriting existing vars

--- bot.rb
+++ bot.rb
@@ -139,7 +139,7 @@ end
 orig_stdout = $stdout
 $stdout = File.new('/dev/null', 'w')
 pid = fork do
-    
+  b = Bot.new(:botname =>  'bot@jabber.thereisnoarizona.org',:host =>  'jabber.thereisnoarizona.org',:password =>  'j4bb3rb0t!', :status_log => '/var/cache/nagios3/status.dat', :cmd_file => '/var/lib/nagios3/rw/nagios.cmd')
   b.run
 end
 ::Process.detach pid
